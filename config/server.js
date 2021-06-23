5ffc9d6a5d7d7aca253f1c7cc9b2ecbc8b9df30f
Kailas Sanap <kailasanap@gmail.com>
2021-06-23 10:38:20 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b2435e94f96610ae6369edd899d057d4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '66296c43-393e-4d7c-8754-711d5db4fb8c'
+};
